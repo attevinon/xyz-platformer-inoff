7bafb443a524d42cac562b59d8482707e201b087
Violetta Lutsevich <violet.lutsevich@gmail.com>
2023-02-07 17:49:06 +0300

    Add project files.

---
+++ Assets/Hero/Scriptes/HeroInputReader.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private HeroScript _hero;
+
+    private void Awake()
+    {
+        _hero = GetComponent<HeroScript>();
+    }
+    void Update()
+    {
+        ///<summary>
+        ///Работает, но движение идёт рывками и нет движения по диагонали
+        ///Раскомментить чтобы протестировать
+        ///</summary>
+        /*GetKeyMovementInput();
+        GetKeyAttackInput();*/
+
+        LegacyAxesInput();
+        LegacyAttackInput();
+    }
+
+    #region LegacyVirtualInput
+    private void LegacyAttackInput()
+    {
+        if (Input.GetButtonUp("Attack"))
+        {
+            _hero.Attack();
+        }
+    }
+
+    private void LegacyAxesInput()
+    {
+        var _horizontal = Input.GetAxis("Horizontal");
+        var _vertical = Input.GetAxis("Vertical");
+        _hero.SetDirection(_horizontal, _vertical);
+    }
+    #endregion
+
+    #region KeyboardManualInput
+    private void GetKeyMovementInput()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            _hero.SetDirection(1, 0);
+        } 
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _hero.SetDirection(-1, 0);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            _hero.SetDirection(0, 1);
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _hero.SetDirection(0, -1);
+
+        }
+        else
+        {
+            _hero.SetDirection(0, 0);
+        }
+    }
+
+    private void GetKeyAttackInput()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            _hero.Attack();
+        }
+    }
+    #endregion
+}
---
+++ Assets/Hero/Scriptes/HeroScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private float _directionX;
+    private float _directionY;
+    void Update()
+    {
+        if (_directionX != 0 || _directionY != 0)
+        {
+            var newXPosition = CountNewPosition(_directionX, transform.position.x);
+            var newYPosition = CountNewPosition(_directionY, transform.position.y);
+
+            transform.position = new Vector3(newXPosition, newYPosition);
+        }
+
+    }
+
+    public void SetDirection(float direction_x, float direction_y)
+    {
+        _directionX = direction_x;
+        _directionY = direction_y;
+    }
+
+    public void Attack()
+    {
+        Debug.Log("ATTACK!!!!!!!!!!");
+    }
+
+    private float CountNewPosition(float direction, float oldPosition)
+    {
+        float delta = direction * _speed * Time.deltaTime;
+        float newPosition = oldPosition + delta;
+
+        return newPosition;
+    }
+}
