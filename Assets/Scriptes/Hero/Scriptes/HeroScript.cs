e1e1f462cb41c6c30677f02c6a524a51e6a3be0d
Violetta Lutsevich <violet.lutsevich@gmail.com>
2023-03-26 12:25:35 +0300

    добавлены и настроены анимации героя

--- Assets/Scriptes/Hero/Scriptes/HeroScript.cs
+++ Assets/Scriptes/Hero/Scriptes/HeroScript.cs
@@ -10,17 +10,24 @@ public class HeroScript : MonoBehaviour
     [SerializeField] private LayerChecker _groundCheker;
 
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
     private Vector2 _direction;
+
+    static readonly int isRunningKey = Animator.StringToHash("is-running");
+    static readonly int isGroundedKey = Animator.StringToHash("is-grounded");
+    static readonly int verticalVelocityKey = Animator.StringToHash("vertical-velocity");
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         Movement();
         Jumping();
+        SetAnimationKeys();
     }
 
     public void SetDirection(Vector2 direction)
@@ -49,6 +56,13 @@ public class HeroScript : MonoBehaviour
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
         }
+    }
+
+    private void SetAnimationKeys()
+    {
+        _animator.SetBool(isRunningKey, _direction.x != 0);
+        _animator.SetBool(isGroundedKey, IsGrounded());
+        _animator.SetFloat(verticalVelocityKey, _rigidbody.velocity.y);
     }
 
     private bool IsGrounded()
