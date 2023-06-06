f2a8ae8d17f1e11827d224c40a2b3c263d9ba254
Violetta Lutsevich <violet.lutsevich@gmail.com>
2023-06-06 18:35:18 +0300

    в enter trigger component добавлен enter event

--- Assets/Scriptes/Components/EnterTriggerComponent.cs
+++ Assets/Scriptes/Components/EnterTriggerComponent.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Events;
+using static EnterCollisionComponent;
 
 public class EnterTriggerComponent : MonoBehaviour
 {
     [SerializeField] private string _tag;
-    [SerializeField] private UnityEvent _action;
+    [SerializeField] private EnterEvent _action;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag(_tag))
-            _action?.Invoke();
+            _action?.Invoke(collision.gameObject);
     }
 }
