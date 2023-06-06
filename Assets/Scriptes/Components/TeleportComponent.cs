25bf63723a422c6cafa27239351a6970e63ff130
Violetta Lutsevich <violet.lutsevich@gmail.com>
2023-06-06 17:23:05 +0300

    телепорт

---
+++ Assets/Scriptes/Components/TeleportComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TeleportComponent : MonoBehaviour
+{
+    [SerializeField] private Transform _destination;
+
+    public void Teleport(GameObject targetToTeleport)
+    {
+        targetToTeleport.transform.position = _destination.position;
+    }
+}
