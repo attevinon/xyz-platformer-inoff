3b8be4cc9dfd080dc81e7d3b764a15bd9f86a440
Violetta Lutsevich <violet.lutsevich@gmail.com>
2023-06-09 16:53:26 +0300

    в enter компоненты можно вводить несколько тегов

--- Assets/Scriptes/Components/EnterCollisionComponent.cs
+++ Assets/Scriptes/Components/EnterCollisionComponent.cs
@@ -4,18 +4,24 @@ using UnityEngine.Events;
 
 public class EnterCollisionComponent : MonoBehaviour
 {
-    [SerializeField] private string _tag;
+    [SerializeField] private string[] _tags;
     [SerializeField] private EnterEvent _action;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag(_tag))
-            _action?.Invoke(collision.gameObject);
+        foreach (var tag in _tags)
+        {
+            if (collision.gameObject.CompareTag(tag))
+            {
+                _action?.Invoke(collision.gameObject);
+                return;
+            }
+        }
     }
+}
 
-    [Serializable]
-    public class EnterEvent : UnityEvent<GameObject>
-    {
+[Serializable]
+public class EnterEvent : UnityEvent<GameObject>
+{
 
-    }
 }
--- Assets/Scriptes/Components/EnterTriggerComponent.cs
+++ Assets/Scriptes/Components/EnterTriggerComponent.cs
@@ -1,16 +1,21 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
-using static EnterCollisionComponent;
 
 public class EnterTriggerComponent : MonoBehaviour
 {
-    [SerializeField] private string _tag;
+    [SerializeField] private string[] _tags;
     [SerializeField] private EnterEvent _action;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag(_tag))
-            _action?.Invoke(collision.gameObject);
+        foreach (var tag in _tags)
+        {
+            if (collision.CompareTag(tag))
+            {
+                _action?.Invoke(collision.gameObject);
+                return;
+            }
+        }
     }
 }
