234733210059b8f5243242d53fbcbd51d4d79cd9
Violetta Lutsevich <violet.lutsevich@gmail.com>
2023-06-09 17:00:12 +0300

    нажимная пластина

---
+++ Assets/Scriptes/Components/ExitCollisionComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ExitCollisionComponent : MonoBehaviour
+{
+    [SerializeField] private string[] _tags;
+    [SerializeField] private EnterEvent _action;
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        foreach (var tag in _tags)
+        {
+            if (collision.gameObject.CompareTag(tag))
+            {
+                _action?.Invoke(collision.gameObject);
+                return;
+            }
+        }
+    }
+}
