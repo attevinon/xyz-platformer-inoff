2cda9c3f6ca38784392036076c1d7fb4aea0291f
attevinon <violet.lutsevich@gmail.com>
2024-07-07 14:59:56 +0300

    добавление классов для инвентаря

---
+++ Assets/Scriptes/Model/Data/InventoryData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrew.Model.Definitions;
+
+namespace PixelCrew.Model.Data
+{
+    [Serializable]
+    public class InventoryData
+    {
+        private List<InventoryItemData> _inventory = new List<InventoryItemData>();
+
+        public void Add(string id, int value)
+        {
+            if (IsNoDef(id)) return; 
+            if (value <= 0) return;
+
+            var item = GetItem(id);
+            if(item == null)
+            {
+                var newItem = new InventoryItemData(id);
+                _inventory.Add(newItem);
+            }
+            item.Value += value;
+        }
+
+        public void Remove(string id, int value)
+        {
+            if (IsNoDef(id)) return;
+            var item = GetItem(id);
+            if (item == null) return;
+
+            item.Value -= value;
+
+            if(item.Value <= 0)
+                _inventory.Remove(item);
+        }
+        
+        public int Count(string id)
+        {
+            int count = 0;
+            foreach (var item in _inventory)
+            {
+                if(item.Id == id)
+                    count += item.Value;
+            }
+            return count;  
+        }
+
+        private InventoryItemData GetItem(string id)
+        {
+            foreach (var item in _inventory)
+            { 
+                if(item.Id == id) return item;
+            }
+
+            return null;
+        }
+
+        private bool IsNoDef(string id)
+        {
+            var itemDef = DefsFacade.I.ItemsDef.Get(id);
+            if (itemDef.IsVoid)
+                Debug.Log($"Definition for item with id={id} is not found");
+
+            return itemDef.IsVoid;
+        }
+    }
+
+    [Serializable]
+    public class InventoryItemData
+    {
+        public string Id;
+        public int Value;
+
+        public InventoryItemData(string id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Assets/Scriptes/Model/Definitions/DefsFacade.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Model.Definitions
+{
+    [CreateAssetMenu(menuName = "Defs/DefsFacade", fileName = "DefsFacade")]
+    public class DefsFacade : ScriptableObject
+    {
+        [SerializeField] private InventoryItemsDef _itemsDef;
+        public InventoryItemsDef ItemsDef => _itemsDef;
+
+        private static DefsFacade _instance;
+        public static DefsFacade I => _instance == null ? LoadDefs() : _instance;
+
+        private static DefsFacade LoadDefs()
+        {
+            return _instance = Resources.Load<DefsFacade>("DefsFacade");
+        }
+    }
+}
