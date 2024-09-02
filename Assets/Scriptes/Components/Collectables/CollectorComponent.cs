d51d06a7ef300fc9be66e20c1d1c983f768464a0
attevinon <violet.lutsevich@gmail.com>
2024-09-02 16:47:20 +0300

    Бочка из которой можно брать айтемы
    Реализация отображает принцип Dependency Invertion

---
+++ Assets/Scriptes/Components/Collectables/CollectorComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using PixelCrew.Model;
+using PixelCrew.Model.Data;
+
+public class CollectorComponent : MonoBehaviour, ICanAddInInventory
+{
+    [SerializeField] private List<InventoryItemData> _items = new List<InventoryItemData>();
+    private GameSession _gameSession;
+
+    public bool TryAddInInventory(string id, int value)
+    {
+        _items.Add(new InventoryItemData(id) { Value = value });
+        return true;
+    }
+
+
+    public void DropInPlayerInventory()
+    {
+        if (_gameSession == null) _gameSession = FindObjectOfType<GameSession>();
+
+        var itemsToDrop = _items.ToArray();
+        foreach (InventoryItemData item in itemsToDrop)
+        {
+            bool isSucces = _gameSession.Data.Inventory.TryAdd(item.Id, item.Value);
+            if (!isSucces) return;
+            _items.Remove(item);
+        }
+        _items.Clear();
+    }
+}
