e63f322b5dfb8a3f02577e0f7a3dfdffe539d469
attevinon <violet.lutsevich@gmail.com>
2024-12-15 19:32:38 +0300

    добавлен нэймспейс

--- Assets/Scriptes/Components/Collectables/CollectorComponent.cs
+++ Assets/Scriptes/Components/Collectables/CollectorComponent.cs
@@ -4,40 +4,43 @@ using PixelCrew.Model;
 using PixelCrew.Model.Data;
 using UnityEngine.Events;
 
-public class CollectorComponent : MonoBehaviour, ICanAddInInventory
+namespace PixelCrew.Components.Collectables
 {
-    [SerializeField] private List<InventoryItemData> _items = new List<InventoryItemData>();
-    [SerializeField] private UnityEvent _onFirstFilled;
-    [SerializeField] private UnityEvent _onEmpty;
-    private GameSession _gameSession;
-
-    public bool TryAddInInventory(string id, int value)
+    public class CollectorComponent : MonoBehaviour, ICanAddInInventory
     {
-        var item = _items.Find(i => i.Id == id);
-        if(item == null)
+        [SerializeField] private List<InventoryItemData> _items = new List<InventoryItemData>();
+        [SerializeField] private UnityEvent _onFirstFilled;
+        [SerializeField] private UnityEvent _onEmpty;
+        private GameSession _gameSession;
+
+        public bool TryAddInInventory(string id, int value)
         {
-            if (_items.Count == 0) _onFirstFilled?.Invoke();
-            item = new InventoryItemData(id);
-            _items.Add(item);
+            var item = _items.Find(i => i.Id == id);
+            if (item == null)
+            {
+                if (_items.Count == 0) _onFirstFilled?.Invoke();
+                item = new InventoryItemData(id);
+                _items.Add(item);
+            }
+            item.Value += value;
+            //имеет ли значение стакаемый ли предмет когда речь идёт о бочке? я думаю нет
+            return true;
         }
-        item.Value += value;
-        //имеет ли значение стакаемый ли предмет когда речь идёт о бочке? я думаю нет
-        return true;
-    }
 
 
-    public void DropInPlayerInventory()
-    {
-        if (_gameSession == null) _gameSession = FindObjectOfType<GameSession>();
-
-        var itemsToDrop = _items.ToArray();
-        foreach (InventoryItemData item in itemsToDrop)
+        public void DropInPlayerInventory()
         {
-            bool isSucces = _gameSession.Data.Inventory.TryAdd(item.Id, item.Value);
-            if (!isSucces) return;
-            _items.Remove(item);
+            if (_gameSession == null) _gameSession = FindObjectOfType<GameSession>();
+
+            var itemsToDrop = _items.ToArray();
+            foreach (InventoryItemData item in itemsToDrop)
+            {
+                bool isSucces = _gameSession.Data.Inventory.TryAdd(item.Id, item.Value);
+                if (!isSucces) return;
+                _items.Remove(item);
+            }
+            _items.Clear();
+            _onEmpty?.Invoke();
         }
-        _items.Clear();
-        _onEmpty?.Invoke();
     }
 }
